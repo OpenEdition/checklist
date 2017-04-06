cbc6a4a4890dc411fd647494742957c79886174a
brrd <contact@brrd.fr>
2017-04-06 16:41:53 +0200

    Init tests using mocha and chai

---
+++ test/tests.js
@@ -0,0 +1,10 @@
+// TODO: ES6 + Babel
+// jshint mocha: true
+
+var expect = window.chai.expect;
+
+describe("hello world", function () {
+  it("should run a stupid test", function () {
+    expect(true).to.equal(true);
+  });
+});
