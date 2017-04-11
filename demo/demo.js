bd5c4b9f869d6a5de3220bed0ec7f39021d604a1
brrd <contact@brrd.fr>
2017-04-11 17:30:12 +0200

    Add checklist.setConfig() and checklist.run()

--- demo/demo.js
+++ demo/demo.js
@@ -1,28 +1,26 @@
-$(function () {
-  checklist({
-    context: function () {
-      return {
-        "article": true,
-        "textes": true,
-        "motsclesfr": $(".motsclesfr .entry").length
-      };
-    },
-    rules: [
-      {
-        name: "Rule Test",
-        condition: "article && textes",
-        action: function () {
-          console.log("Processing Test...");
-          this.notify({
-            name: "First statement",
-            description: "This is a first statement for testing purpose...",
-            count: 2
-          });
-          this.notify();
-          this.notify();
-          this.resolve("Test result");
-        }
+checklist.setConfig({
+  context: function () {
+    return {
+      "article": true,
+      "textes": true,
+      "motsclesfr": $(".motsclesfr .entry").length
+    };
+  },
+  rules: [
+    {
+      name: "Rule Test",
+      condition: "article && textes",
+      action: function () {
+        console.log("Processing Test...");
+        this.notify({
+          name: "First statement",
+          description: "This is a first statement for testing purpose...",
+          count: 2
+        });
+        this.notify();
+        this.notify();
+        this.resolve("Test result");
       }
-    ]
-  });
+    }
+  ]
 });
--- src/main.js
+++ src/main.js
@@ -1,19 +1,31 @@
 const UI = require("./ui.js");
 const Checker = require("./checker.js");
 
-window.checklist = function ({rules, context, parent = "#container"}) {
-  if (typeof jQuery === "undefined") {
-    throw Error ("Checklist requires jQuery");
+window.checklist = {
+  run: function (config) {
+    if (typeof jQuery === "undefined") {
+      throw Error ("Checklist requires jQuery");
+    }
+
+    if (config) {
+      this.setConfig(config);
+    }
+    const {rules, context, parent} = this.config;
+
+    // UI
+    const ui = new UI({parent});
+    ui.show();
+
+    // Checker
+    const checker = new Checker({ rules, context });
+    checker.on("done", () => {
+      ui.inject(checker.statements);
+    });
+    checker.run();
+  },
+
+  // TODO: 1. merge config, 2. add an "override" parameter
+  setConfig: function ({rules, context, parent = "#container"}) {
+    this.config = {rules, context, parent};
   }
-
-  // UI
-  const ui = new UI({parent});
-  ui.show();
-
-  // Checker
-  const checker = new Checker({ rules, context });
-  checker.on("done", () => {
-    ui.inject(checker.statements);
-  });
-  checker.run();
 };
