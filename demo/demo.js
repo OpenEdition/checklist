e16ed00e6196713d19198011437db32604eaf783
brrd <contact@brrd.fr>
2017-04-11 16:48:59 +0200

    Move demo config and exec into demo/demo.js

---
+++ demo/demo.js
@@ -0,0 +1,28 @@
+$(function () {
+  checklist({
+    context: function () {
+      return {
+        "article": true,
+        "textes": true,
+        "motsclesfr": $(".motsclesfr .entry").length
+      };
+    },
+    rules: [
+      {
+        name: "Rule Test",
+        condition: "article && textes",
+        action: function () {
+          console.log("Processing Test...");
+          this.notify({
+            name: "First statement",
+            description: "This is a first statement for testing purpose...",
+            count: 2
+          });
+          this.notify();
+          this.notify();
+          this.resolve("Test result");
+        }
+      }
+    ]
+  });
+});
