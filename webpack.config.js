d2fbce8b146ff995ba054be92a2d77ce627ee416
brrd <contact@brrd.fr>
2017-04-11 16:18:53 +0200

    Rename app.js into main.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   context: path.resolve(__dirname, "./src"),
   entry: {
-    app: "./app.js",
+    app: "./main.js",
   },
   output: {
     path: path.resolve(__dirname, "./dist"),
