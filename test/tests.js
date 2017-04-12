c57af90ae8e7f7f816b4c37e4961eb3349d59c92
brrd <contact@brrd.fr>
2017-04-12 11:29:06 +0200

    Merge test/config.js into test/tests.js and use hooks to load checklist

--- test/tests.js
+++ test/tests.js
@@ -1,14 +1,48 @@
-// TODO: ES6 + Babel
 // jshint mocha: true
-
 var expect = window.chai.expect;
 
-// Get testFlags
+// Functions to store and get stuffs
+function setFlag (name, value) {
+  if (window.testFlags == null) {
+    window.testFlags = {};
+  }
+  window.testFlags[name] = typeof value !== "undefined" ? value : true;
+}
+
 function getFlag (name) {
     return window.testFlags[name];
 }
 
+// Tests
 describe("Context and conditions", function () {
+  before(function() {
+    checklist.run({
+      context: function () {
+        return {
+          yes: true,
+          no: false
+        };
+      },
+      rules: [
+        {
+          name: "Context is true (function)",
+          condition: (context) => context.yes && !context.no,
+          action: () => setFlag("context-true-function")
+        },
+        {
+          name: "Context is true (string)",
+          condition: "yes && !no",
+          action: () => setFlag("context-true-string")
+        },
+        {
+          name: "Context is false",
+          condition: "no",
+          action: () => setFlag("context-false")
+        }
+      ]
+    });
+  });
+
   it("Should run action() when condition (function) is true", function () {
     var flag = getFlag("context-true-function");
     expect(flag).to.equal(true);
