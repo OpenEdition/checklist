0e9e358615dfa8184d08b3451f9a379cfc85be83
brrd <contact@brrd.fr>
2017-04-19 17:24:30 +0200

    [Tests] Use a separate checker in each test

--- test/tests.js
+++ test/tests.js
@@ -1,18 +1,6 @@
 // jshint mocha: true
 var expect = window.chai.expect;
 
-// Functions to store and get stuffs
-function setFlag (name, value) {
-  if (window.testFlags == null) {
-    window.testFlags = {};
-  }
-  window.testFlags[name] = typeof value !== "undefined" ? value : true;
-}
-
-function getFlag (name) {
-    return window.testFlags[name];
-}
-
 // Tests
 describe("Initialization and execution", function () {
   it("Should return a Checker instance", function () {
@@ -52,7 +40,7 @@ describe("Initialization and execution", function () {
 });
 
 describe("Context and conditions", function () {
-  before(function() {
+  it("Should run action() when condition (function) is true", function (done) {
     checklist.start({
       parent: false,
       context: {
@@ -63,35 +51,47 @@ describe("Context and conditions", function () {
         {
           name: "Context is true (function)",
           condition: (context) => context.yes && !context.no,
-          action: () => setFlag("context-true-function")
-        },
-        {
-          name: "Context is true (string)",
-          condition: "yes && !no",
-          action: () => setFlag("context-true-string")
-        },
-        {
-          name: "Context is false",
-          condition: "no",
-          action: () => setFlag("context-false")
+          action: () => done()
         }
       ]
     });
   });
 
-  it("Should run action() when condition (function) is true", function () {
-    var flag = getFlag("context-true-function");
-    expect(flag).to.equal(true);
-  });
-
-  it("Should run action() when condition (string) is true", function () {
-    var flag = getFlag("context-true-string");
-    expect(flag).to.equal(true);
+  it("Should run action() when condition (string) is true", function (done) {
+    checklist.start({
+      parent: false,
+      context: {
+        yes: true,
+        no: false
+      },
+      rules: [
+        {
+          name: "Context is true (string)",
+          condition: "yes && !no",
+          action: () => done()
+        }
+      ]
+    });
   });
 
   it("Should not run action() when condition is false", function () {
-    var flag = getFlag("context-false");
-    expect(flag).to.equal(undefined);
+    var flag = false;
+    checklist.start({
+      parent: false,
+      context: {
+        yes: true,
+        no: false
+      },
+      rules: [
+        {
+          name: "Context is false",
+          condition: "no",
+          action: () => flag = true
+        }
+      ]
+    });
+    // FIXME: this test should be async
+    expect(flag).to.equal(false);
   });
 
   it("Should compute context when it's a function", function (done) {
