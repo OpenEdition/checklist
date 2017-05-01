30b8076e2a7a86a6ba5a8e7b8ab284cd2474d8df
brrd <contact@brrd.fr>
2017-05-01 15:20:38 +0200

    Add states to base class

--- src/base.js
+++ src/base.js
@@ -3,6 +3,20 @@ const EventEmitter = require("eventemitter2").EventEmitter2;
 class Base extends EventEmitter {
   constructor () {
     super();
+    this.states = {};
+  }
+
+  setState(states) {
+    Object.assign(this.states, states);
+    return this;
+  }
+
+  getState(state) {
+    return this.states[state];
+  }
+
+  hasState(state) {
+    return this.getState(state) === true;
   }
 }
 
