d72e5203eeb939f086bc383b830b4fa0a4db4442
brrd <contact@brrd.fr>
2017-05-04 11:25:44 +0200

    Move app main logic into a new Checklist class

---
+++ src/checklist.js
@@ -0,0 +1,69 @@
+const Base = require("./base.js");
+const Batch = require("./batch.js");
+const Checker = require("./checker.js");
+const Loader = require("./loader.js");
+const UI = require("./ui.js");
+
+class Checklist extends Base {
+  constructor (userConfig) {
+    super("Checklist");
+    this.createConfig(userConfig);
+  }
+
+  createConfig (config) {
+    this.config = {};
+    this.setConfig(config);
+  }
+
+  createLoader (force = false) {
+    if (this.loader && !force) return;
+    this.loader = new Loader();
+    // TODO: return promise
+  }
+
+  // FIXME: missing parent
+  createUi (force = false) {
+    if (this.ui && !force) return;
+    this.ui = new UI({parent})
+      .show();
+    // TODO: return promise
+  }
+
+  check (rules = this.userConfig.rules) {
+    // TODO: rename context in contextCreator
+    const {context, ui} = this;
+    const checker = new Checker({ rules, context });
+    if (ui) {
+      checker.on("done", (statements) => ui.inject(statements));
+    }
+    return new Promise((resolve, reject) => {
+      checker.on("done", () => resolve(checker));
+      // TODO: handle error
+      // checker.on("err", () => reject());
+    });
+  }
+
+  // TODO: 1. merge config, 2. add an "override" parameter
+  setConfig (options) {
+    if (!options) return;
+    const config = {};
+    config.context = options.context || this.config.context;
+    config.rules = options.rules || this.config.rules;
+    config.parent = options.parent || this.config.parent;
+    this.config = config;
+    return this;
+  }
+
+  clear () {
+    this.config = {};
+    this.createLoader(true);
+    // TODO: Ui ?
+  }
+
+  batch (hrefs) {
+    const batch = new Batch(hrefs);
+    return batch;
+  }
+}
+
+module.exports = Checklist;
--- src/main.js
+++ src/main.js
@@ -1,78 +1,8 @@
-const Batch = require("./batch.js");
-const Checker = require("./checker.js");
-const Loader = require("./loader.js");
-const UI = require("./ui.js");
+const Checklist = require("./checklist.js");
 
 if (typeof jQuery === "undefined") {
   throw Error ("Checklist requires jQuery");
 }
 
-window.checklist = {
-  // checklist properties
-  config: window.checklistUserConfig || {},
-
-  // checklist methods
-  // TODO: 1. merge config, 2. add an "override" parameter
-  setConfig: function (options) {
-    const config = {};
-    config.context = options.context || this.config.context;
-    config.rules = options.rules || this.config.rules;
-    config.parent = options.parent || this.config.parent;
-    this.config = config;
-  },
-
-  init: function (config) {
-    const initUi = (ui) => {
-      if (!ui) {
-        ui = new UI({parent});
-        ui.show();
-      }
-      checker.on("done", (statements) => {
-        ui.inject(statements);
-      });
-    };
-
-    if (config) {
-      this.setConfig(config);
-    }
-
-    const {rules, context, parent} = this.config;
-
-    if (!this.loader) {
-      this.loader = new Loader();
-    }
-
-    const checker = new Checker({ rules, context });
-    this.checker = checker;
-
-    // Init optional UI
-    if (parent) {
-      initUi(this.ui);
-    }
-
-    return checker;
-  },
-
-  start: function (config, callback) {
-    const checker = this.init(config);
-
-    if (typeof callback === "function") {
-      checker.on("done", (statements) => {
-        callback(checker, statements);
-      });
-    }
-
-    checker.run();
-    return checker;
-  },
-
-  clear: function () {
-    this.config = {};
-    this.loader = new Loader();
-  },
-
-  batch: function (hrefs) {
-    const batch = new Batch(hrefs);
-    return batch;
-  }
-};
+const userConfig = window.checklistUserConfig;
+window.checklist = new Checklist(userConfig);
