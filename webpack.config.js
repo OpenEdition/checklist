ad0a161c2661c5e8150f37305dbf3aa2a65c9680
brrd <contact@brrd.fr>
2017-05-08 18:03:30 +0200

    [Tests] Build the new tests with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,44 @@
 const path = require("path");
 
-module.exports = {
-  context: path.resolve(__dirname, "./src"),
-  entry: {
-    app: "./main.js",
-  },
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "[name].bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: [/node_modules/],
-        use: [{
-          loader: "babel-loader",
-          options: { presets: ["es2015"] }
-        }],
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
+const jsRule = {
+  test: /\.js$/,
+  exclude: [/node_modules/],
+  use: [{
+    loader: "babel-loader",
+    options: { presets: ["es2015"] }
+  }],
 };
+
+const cssRule = {
+  test: /\.css$/,
+  use: ["style-loader", "css-loader"],
+};
+
+module.exports = [
+  {
+    context: path.resolve(__dirname, "./src"),
+    entry: {
+      app: "./main.js",
+    },
+    output: {
+      path: path.resolve(__dirname, "./dist"),
+      filename: "checklist.js"
+    },
+    module: {
+      rules: [jsRule, cssRule],
+    }
+  },
+  {
+    context: path.resolve(__dirname, "./test/src"),
+    entry: {
+      app: "./main.js",
+    },
+    output: {
+      path: path.resolve(__dirname, "./test/dist"),
+      filename: "tests.js"
+    },
+    module: {
+      rules: [jsRule],
+    }
+  }
+];
