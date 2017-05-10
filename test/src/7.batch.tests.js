4315d252b7706a65457d23f0054315305dbed54f
brrd <contact@brrd.fr>
2017-05-10 14:59:25 +0200

    [Tests] Use new Batch API

--- test/src/7.batch.tests.js
+++ test/src/7.batch.tests.js
@@ -11,32 +11,24 @@ describe("Batch", function () {
     });
   });
 
-  it("Should return an instance of Batch", function () {
-    expect(batch).to.have.property("classname", "Batch");
-  });
+  describe("Execution", function () {
 
-  it("Should return a promise", function () {
-    const res = batch.init();
-    expect(res).to.be.a("promise");
-  });
+    it("Should return an instance of Batch", function () {
+      expect(batch).to.have.property("classname", "Batch");
+    });
 
-  // TODO: rename events in a consistant way and move this test to the "events" section
-  it("Should emit the 'ready' event", function (done) {
-    batch.once("ready", () => done());
-    batch.init();
-  });
-
-  it("Should get and store checkers", function (done) {
-    batch = checklist.runBatch([remoteLocation, remoteLocation2]);
-    batch.once("ready", () => {
-      expectAsync(done, () => {
-        expect(batch).to.have.property("checkers");
-        expect(batch.checkers).to.be.an("array");
-        expect(batch.checkers).to.have.lengthOf(2);
-        expect(batch.checkers[0]).to.have.property("classname", "Checker");
+    it("Should get and store checkers", function (done) {
+      checklist.runBatch([remoteLocation, remoteLocation2])
+      .then((batch) => {
+        expectAsync(done, () => {
+          expect(batch).to.have.property("checkers");
+          expect(batch.checkers).to.be.an("array");
+          expect(batch.checkers).to.have.lengthOf(2);
+          expect(batch.checkers[0]).to.have.property("classname", "Checker");
+        });
       });
     });
-    batch.init();
+
   });
 
 });
