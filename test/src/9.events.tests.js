c2fc3bc9df890ec82e01decf74f5c3a863fe978a
brrd <contact@brrd.fr>
2017-05-11 17:52:56 +0200

    [Tests] Move event handlers in a separate function

--- test/src/9.events.tests.js
+++ test/src/9.events.tests.js
@@ -1,72 +1,81 @@
 describe("Events", function () {
 
-  it("Should emit the 'checker.done' event", function (done) {
-    checklist.once("checker.done", (checker) => {
-      expectAsync(done, () => {
-        expect(checker).to.have.property("classname", "Checker");
-      });
-    });
-    checklist.run();
-  });
-
-  it("Should emit the 'check.done' event with an argument", function (done) {
-    checklist.once("check.done", (check) => {
-      expectAsync(done, () => {
-        expect(check).to.have.property("classname", "Check");
-      });
-    });
-    checklist.run({
-      name: "This should run",
-      action: function () {
-        this.resolve();
+  function getHandler (eventName, done) {
+    const handlers = {
+      "checker.done": (checker) => {
+        expectAsync(done, () => {
+          expect(checker).to.have.property("classname", "Checker");
+        });
+      },
+      "check.done+success": (check) => {
+        expectAsync(done, () => {
+          expect(check).to.have.property("classname", "Check");
+        });
+      },
+      "check.rejected": (err, check) => {
+        expectAsync(done, () => {
+          expect(err).to.be.an("error");
+          expect(check).to.have.property("classname", "Check");
+        });
+      },
+      "statement": (statement) => {
+        expectAsync(done, () => {
+          expect(statement).to.not.be.undefined;
+          expect(statement).to.have.property("classname", "Statement");
+        });
       }
-    });
-  });
+    };
+    return handlers[eventName];
+  }
 
-  it("Should emit the 'check.success' event with an argument", function (done) {
-    // FIXME: same code than the previous test
-    checklist.once("check.success", (check) => {
-      expectAsync(done, () => {
-        expect(check).to.have.property("classname", "Check");
+  describe("Checker", function () {
+
+    it("Should emit the 'checker.done' event", function (done) {
+      checklist.once("checker.done", getHandler("checker.done", done));
+      checklist.run();
+    });
+
+    it("Should emit the 'check.done' event with an argument", function (done) {
+      checklist.once("check.done", getHandler("check.done+success", done));
+      checklist.run({
+        name: "This should run",
+        action: function () {
+          this.resolve();
+        }
       });
     });
-    checklist.run({
-      name: "This should run",
-      action: function () {
-        this.resolve();
-      }
-    });
-  });
 
-  // FIXME: this test leads to an error message in the browser console
-  it("Should emit the 'check.rejected' event with two arguments", function (done) {
-    checklist.once("check.rejected", (err, check) => {
-      expectAsync(done, () => {
-        expect(err).to.be.an("error");
-        expect(check).to.have.property("classname", "Check");
+    it("Should emit the 'check.success' event with an argument", function (done) {
+      checklist.once("check.done", getHandler("check.done+success", done));
+      checklist.run({
+        name: "This should run",
+        action: function () {
+          this.resolve();
+        }
       });
     });
-    checklist.run({
-      name: "This should be rejected",
-      action: function () {
-        this.reject("Rejection message");
-      }
-    });
-  });
 
-  it("Should emit the 'statement' event with an argument", function (done) {
-    checklist.once("statement", (statement) => {
-      expectAsync(done, () => {
-        expect(statement).to.not.be.undefined;
-        expect(statement).to.have.property("classname", "Statement");
+    // FIXME: this test leads to an error message in the browser console
+    it("Should emit the 'check.rejected' event with two arguments", function (done) {
+      checklist.once("check.rejected", getHandler("check.rejected", done));
+      checklist.run({
+        name: "This should be rejected",
+        action: function () {
+          this.reject("Rejection message");
+        }
       });
     });
-    checklist.run({
-      name: "This should run",
-      action: function () {
-        this.resolve(true);
-      }
+
+    it("Should emit the 'statement' event with an argument", function (done) {
+      checklist.once("statement", getHandler("statement", done));
+      checklist.run({
+        name: "This should run",
+        action: function () {
+          this.resolve(true);
+        }
+      });
     });
+
   });
 
 });
