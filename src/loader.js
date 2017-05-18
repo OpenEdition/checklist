813468b3852b966476e769a692530dc2e38cd5f1
brrd <contact@brrd.fr>
2017-05-18 18:28:17 +0200

    Improve Loader.requestSource() performance and readability

--- src/loader.js
+++ src/loader.js
@@ -1,39 +1,36 @@
 const Base = require("./base.js");
 const Source = require("./source.js");
 
-function addSource (loader, href) {
-  return new Promise((resolve, reject) => {
-    const source = new Source({ href, caller: loader });
-    loader.sources.push(source);
-    // TODO: return the source and manage all of them with whenState() (see requestSource)
-    source.on("ready", () => resolve(source));
-    source.on("failed", (err)=> reject(err));
-    source.load();
-  });
+function getNewSource (loader, href) {
+  const source = new Source({ href, caller: loader });
+  source.load();
+  return source;
+}
+
+function registerSource (loader, source) {
+  loader.sources.push(source);
+  return source;
 }
 
 class Loader extends Base {
   constructor ({ caller }) {
     super("Loader", caller);
 
-    this.sources = [];
-    // Add self
-    addSource(this)
-      .then((source) => {
-        this.selfSource = source;
-        this.triggerState("ready");
-      });
+    this.selfSource = getNewSource(this);
+    this.sources = [this.selfSource];
+    this.triggerState("ready");
   }
 
   requestSource (href) {
     return new Promise((resolve, reject) => {
-      const found = this.getSource(href);
-      if (!found) {
-        return addSource(this, href).then(resolve, reject);
-      }
-      found.whenState("ready").then(() => resolve(found));
-      // FIXME: Error message is not passed as it is with addSource (which is used when the source is created for the first time). This is because states are just booleans. Error message should be stored somehow in Source.
-      found.whenState("failed").then(() => reject(Error("Source not found")));
+      const source = this.getSource(href) || registerSource(this, getNewSource(this, href));
+
+      source.whenState("ready").then(() => {
+        if (source.hasState("success")) {
+          return resolve(source);
+        }
+        reject(source.error);
+      });
     });
   }
 
