81aaa8bb0090d332dd557a942f6ab6a7a07a5efb
Thomas Brouard <contact@brrd.fr>
2017-06-13 17:56:05 +0200

    Use assignAttributes() in Statement

--- src/statement.js
+++ src/statement.js
@@ -4,17 +4,31 @@ function getIdFromName (name) {
   return name.replace(/\W/gi, "-").toLowerCase();
 }
 
+// Get values from check, otherwise default values are inherited from check
+function assignAttributes ({attributes, statement, infos, check}) {
+  attributes.forEach((attr) => {
+    statement[attr] = infos[attr] || check[attr];
+  });
+}
+
 class Statement extends Base {
   constructor ({check, infos, caller}) {
     super("Statement", caller);
     this.check = check;
     this.docId = check.docId;
 
-    // Get values from check, otherwise default values are inherited from check
-    this.name = infos.name || check.name;
-    this.description = infos.description || check.description;
-    this.id = infos.id || check.id || getIdFromName(this.name);
+    assignAttributes({
+      attributes: ["name", "description", "id"],
+      statement: this,
+      infos,
+      check
+    });
     this.count = 1;
+
+    // If no id then create it from name
+    if (this.id == null) {
+      this.id = getIdFromName(this.name);
+    }
 
     // If infos is a string, then use it as the name
     if (typeof infos === "string") {
