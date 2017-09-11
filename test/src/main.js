ec5ee5c0dab2873c6ab548669ed2b069750e7d18
Thomas Brouard <contact@brrd.fr>
2017-09-11 15:39:08 +0200

    [Tests] Don't run tests if checklist not active

--- test/src/main.js
+++ test/src/main.js
@@ -1,8 +1,28 @@
-function importAll (r) {
-  r.keys().forEach(r);
-}
+(function () {
+  // Don't run tests if Checklist is not active
+  const on = localStorage.getItem("checklist-on");
+  if (on === false || on === "false") {
+    // Handler
+    const setActive = () => {
+      localStorage.setItem("checklist-on", true);
+      location.reload();
+    };
+    // Create element
+    const $div = $("<div class='checklist-off'><p>Please activate Checklist and reload this page</p></div>");
+    $("<button class='activate-checklist'>Activate</button>").click(setActive).appendTo($div);
+    // Ready
+    $(function () {
+      $("#mocha").append($div);
+    });
+    return;
+  }
 
-require("./vars.js");
-require("./utils.js");
+  function importAll (r) {
+    r.keys().forEach(r);
+  }
 
-importAll(require.context('../src/', false, /tests\.js$/));
+  require("./vars.js");
+  require("./utils.js");
+
+  importAll(require.context('../src/', false, /tests\.js$/));
+})();
