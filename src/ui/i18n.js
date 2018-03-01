1bf04d93cedabb4735beeb2375e19e788c5f55a9
Thomas Brouard <contact@brrd.fr>
2018-03-01 18:29:29 +0100

    Add locales in locales/ dir

--- src/ui/i18n.js
+++ src/ui/i18n.js
@@ -1,10 +1,23 @@
 const i18next = require("i18next");
 
+function getLocales () {
+  const locales = {};
+  const r = require.context('./locales/', false, /\.json$/);
+  r.keys().forEach((key) => {
+    const re = /[A-z]+(?=\.json$)/;
+    const lang = (re.exec(key) || [])[0];
+    if (lang == null) return;
+    locales[lang] = r(key);
+  });
+  return locales;
+}
+
 i18next.init({
   fallbackLng: "en",
   // TODO: load lng from config
   lng: "fr",
   debug: false,
+  resources: getLocales(),
 }, function(err, t) {
   // initialized and ready to go!
   console.log("i18next ready!");
