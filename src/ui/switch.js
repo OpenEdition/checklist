965b7c19227cad3f812980f9e8f4447be7e03733
Thomas Brouard <contact@brrd.fr>
2018-04-30 11:39:07 +0200

    Fix t() not working in switch.js

--- src/ui/switch.js
+++ src/ui/switch.js
@@ -1,5 +1,3 @@
-// FIXME: charger switch depuis UI car 1) c'est pas logique autrement, 2) on a besoin de ui.t() dans switch
-
 function startChecklist () {
   localStorage.setItem("checklist-on", true);
   location.reload();
@@ -9,7 +7,7 @@ function showSwitch () {
   // FIXME: window.t() n'existe pas, utiliser ui.t()
   const html = `
     <div id="checklist-start" class="checklist-start">
-      <span>${t("checklist-activate")}</span>
+      <span>Checklist</span>
       <div class="checklist-switch">
         <div class="checklist-slider"></div>
       </div>
