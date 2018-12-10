OpenEdition/checklist

commit b154f09d3a9aa743ddca7658e5c2560847fad22c

Don't load check source when check is "dropped"
e.g. context doesn't match condition

src/check.js
@@ -23,6 +23,12 @@ class Check extends Base {
     this.assign(allowedProps, rule, {context, docId, source});
     this.statements = [];
 
+    if (!this.test()) {
+      this.triggerState("dropped", this);
+      this.triggerState("ready");
+      return;
+    }
+
     getSource(this)
     .then((source) => {
       this.source = source;
@@ -78,22 +84,21 @@ class Check extends Base {
       return this;
     }
 
-    if (!this.test()) {
-      this.resolve();
-      return this;
-    }
+    if (this.hasState("run")) return this;
 
-    if (!this.hasState("run")) {
+    this.triggerState("run", this);
+
+    if (!this.hasState("dropped")) {
       // TODO: move delay in config
       // TODO: do something/run event when timeout
       const delay = 3000;
-      this.triggerState("run", this);
       setTimeout(this.resolve.bind(this), delay);
       const selectFunc = this.source.get$();
       const bodyClasses = this.source.bodyClasses;
       this.action.call(this, selectFunc, bodyClasses);
-      return this;
     }
+
+    return this.resolve();
   }
 
   test () {

src/checker.js
@@ -78,6 +78,7 @@ class Checker extends Base {
         {"done": "check.done"},
         {"success": "check.success"},
         {"rejected": "check.rejected"},
+        {"dropped": "check.dropped"},
         "statement.new",
         "statement.update",
         "marker"

src/checklist.js
@@ -58,7 +58,7 @@ class Checklist extends Base {
     }
 
     const forwardCheckerEvents = (checker) => {
-      const events = [ "check.run", "check.done", "check.success", "check.rejected", "statement.new", "statement.update", "checker.run", "checker.done", "marker"];
+      const events = [ "check.run", "check.done", "check.success", "check.rejected", "check.dropped", "statement.new", "statement.update", "checker.run", "checker.done", "marker"];
       this.forwardEvents(checker, events);
     };
 

commit 97a72e2bedc189f604b28e4c52d9dc9b2ed95beb

Fix: check.resolve() must remain async
...otherwise check doesnt wait for action to resolve.
This bug was introduced in b154f09.

src/check.js
@@ -85,20 +85,17 @@ class Check extends Base {
     }
 
     if (this.hasState("run")) return this;
-
     this.triggerState("run", this);
+    if (this.hasState("dropped")) return this.resolve();
 
-    if (!this.hasState("dropped")) {
-      // TODO: move delay in config
-      // TODO: do something/run event when timeout
-      const delay = 3000;
-      setTimeout(this.resolve.bind(this), delay);
-      const selectFunc = this.source.get$();
-      const bodyClasses = this.source.bodyClasses;
-      this.action.call(this, selectFunc, bodyClasses);
-    }
-
-    return this.resolve();
+    // TODO: move delay in config
+    // TODO: do something/run event when timeout
+    const delay = 3000;
+    setTimeout(this.resolve.bind(this), delay);
+    const selectFunc = this.source.get$();
+    const bodyClasses = this.source.bodyClasses;
+    this.action.call(this, selectFunc, bodyClasses);
+    return this;
   }
 
   test () {